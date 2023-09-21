b7d52d47d5aef97a884afa2b75a50d9a53c60e79
junyoung song <s8092jy@gmail.com>
2023-09-21 18:00:33 +0900

    pp120

--- pp120/pp120/main.cpp
+++ pp120/pp120/main.cpp
@@ -7,11 +7,11 @@ int main(void)
 {
 	double weight = 0.0;
 	double height = 0.0;
-	double bmi = ((weight) / ((height) / 100.0) * ((height) / 100.0));
+	
 	
 	printf("몸무게(kg)와 키(cm) 입력 : ");
 	scanf_s("%lf%lf", &weight, &height);
-	
+	double bmi = ((weight) / ((height) / 100.0) * ((height) / 100.0));
 	
 	((bmi) >= 20) && ((bmi) < 25) ? printf("표준입니다.") : printf("체중관리가 필요합니다.");
 
