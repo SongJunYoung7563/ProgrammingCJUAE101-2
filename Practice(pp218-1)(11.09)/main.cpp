384bcf5316173970397e47da0c1d41a8703a6f36
junyoung song <s8092jy@gmail.com>
2023-11-09 16:34:10 +0900

    Pracitce(pp218-1)(11.09)

---
+++ Practice(pp218-1)(11.09)/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define N1 5
+#define N2 10
+#define N3 3
+#define N4 4
+int main()
+{
+	//1-1
+	int ary1[N1] = {};
+	//1-2
+	double ary2[N2] = {};
+	//1-3
+	int ary3[] = { 1,2,3 };
+	//1-4
+	char ary4[N4] = {1,2,3,4};
+
+	return 0;
+}
